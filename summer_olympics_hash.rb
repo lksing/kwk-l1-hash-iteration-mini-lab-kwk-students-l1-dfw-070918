882e77ae931fb238ddb25c63ed5991748eb7c49e
lksing <leiaxsing@gmail.com>
2018-07-11 19:16:19 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -55,7 +55,7 @@ def iterate_through_keys
   
   summer_olympics[:Atlanta] = "1996"
   summer_olympic_cities[]
-  summer_olympics.keys = summer_olympic_cities
+  summer_olympics.each_key = summer_olympic_cities
   summer_olympics.each_key do |upcase_cities|
     summer_olympic_cities << cities.upcase
 
